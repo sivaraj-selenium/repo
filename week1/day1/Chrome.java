4254a43d24b18daef01af4da348f51cf4fbe17f2
Sivaraj <sivarajece09@gmail.com>
2025-12-01 00:20:59 +0530

    Add files via upload

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		System.out.println("version" + "=" + version);
+		String developer = "Google";
+		System.out.println("developer" + "=" + developer);
+		boolean isBeta = false;
+		System.out.println("isBeta" + "=" + isBeta);
+		int releaseYear = 2008;
+		System.out.println("releaseYear" + "=" + releaseYear);
+		char shortcutKey = 'C';
+		System.out.println("shortcutKey" + "=" + shortcutKey);
+
+	}
+
+}
---
+++ week1/day2/PrimeNumber.java
@@ -0,0 +1,12 @@
+package week1.day2;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		for (int i = 2; i < 10; i++) {
+			
+	
+}
+	}
+
+}
